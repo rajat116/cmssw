3c17069331806841b92a8ac285dc9a8cc7e89a66
Rajat Gupta <rajatgupta116@gmail.com>
2016-12-11 10:51:47 +0100

    latest

--- Validation/MuonME0Validation/interface/ME0DigisValidation.h
+++ Validation/MuonME0Validation/interface/ME0DigisValidation.h
@@ -1,32 +1,153 @@
-#ifndef ME0DigisValidation_H
-#define ME0DigisValidation_H
+//#ifndef ME0DigisValidation_H
+//#define ME0DigisValidation_H
 
 #include "Validation/MuonME0Validation/interface/ME0BaseValidation.h"
-//Data Formats
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+//Data Format
+//#include "DataFormats/GEMDigi/interface/ME0DigiCollection.h"
 #include "DataFormats/GEMDigi/interface/ME0DigiPreReco.h"
 #include "DataFormats/GEMDigi/interface/ME0DigiPreRecoCollection.h"
+#include "DataFormats/MuonDetId/interface/GEMDetId.h"
+#include "DataFormats/GeometrySurface/interface/LocalError.h"
+#include "DataFormats/GeometryVector/interface/LocalPoint.h"
+#include "DataFormats/Scalers/interface/DcsStatus.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/Math/interface/deltaPhi.h"
 
+#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
+#include "SimDataFormats/Track/interface/SimTrackContainer.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
 
-class ME0DigisValidation : public ME0BaseValidation
+#include "Geometry/Records/interface/MuonGeometryRecord.h"
+#include "Geometry/CommonDetUnit/interface/GeomDet.h"
+#include "Geometry/GEMGeometry/interface/ME0Geometry.h"
+#include "Geometry/GEMGeometry/interface/GEMEtaPartition.h"
+#include "Geometry/GEMGeometry/interface/GEMEtaPartitionSpecs.h"
+#include "Geometry/CommonTopologies/interface/StripTopology.h"
+
+//Log messages
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+using namespace std;
+
+struct MyME0SimHit
 {
-public:
-  explicit ME0DigisValidation( const edm::ParameterSet& );
-  ~ME0DigisValidation();
-  void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
-  void analyze(const edm::Event& e, const edm::EventSetup&) override;
- private:
+    Int_t eventNumber;
+    Int_t detUnitId, particleType;
+    Float_t x, y, energyLoss, pabs, timeOfFlight;
+    Int_t region, ring, station, layer, chamber, roll;
+    Float_t globalR, globalEta, globalPhi, globalX, globalY, globalZ;
+    Int_t strip;
+    Float_t Phi_0, DeltaPhi, R_0;
+    Int_t countMatching;
+};
 
-  MonitorElement* me0_strip_dg_xy[2][6];
-  MonitorElement* me0_strip_dg_xy_Muon[2][6];
-  MonitorElement* me0_strip_dg_zr[2][6];
-  MonitorElement* me0_strip_dg_zr_tot[2];
-  MonitorElement* me0_strip_dg_zr_tot_Muon[2];
+struct MyME0Digi
+{
+    Int_t detId, particleType;
+    Short_t region, ring, station, layer, chamber, roll;
+    Short_t strip, bx;
+    Float_t x, y;
+    Float_t g_r, g_eta, g_phi, g_x, g_y, g_z;
+    Float_t x_sim, y_sim;
+    Float_t g_eta_sim, g_phi_sim, g_x_sim, g_y_sim, g_z_sim;
+};
 
-  edm::EDGetToken InputTagToken_Digi;
+struct MySimTrack
+{
+    Float_t pt, eta, phi;
+    Char_t charge;
+    Char_t endcap;
+    Char_t gem_sh_layer1, gem_sh_layer2;
+    Char_t gem_dg_layer1, gem_dg_layer2;
+    Char_t gem_pad_layer1, gem_pad_layer2;
+    Float_t gem_sh_eta, gem_sh_phi;
+    Float_t gem_sh_x, gem_sh_y;
+    Float_t gem_dg_eta, gem_dg_phi;
+    Float_t gem_pad_eta, gem_pad_phi;
+    Float_t gem_lx_even, gem_ly_even;
+    Float_t gem_lx_odd, gem_ly_odd;
+    Char_t  has_gem_sh_l1, has_gem_sh_l2;
+Char_t  has_gem_dg_l1, has_gem_dg_l2;
+    Char_t  has_gem_pad_l1, has_gem_pad_l2;
+    Float_t gem_trk_eta, gem_trk_phi, gem_trk_rho;
+};
 
-  Int_t npart;
 
+class ME0DigisValidation : public edm::EDAnalyzer {
+   public:
+      explicit ME0DigisValidation(const edm::ParameterSet&);
+      ~ME0DigisValidation();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      bool isSimTrackGood(const SimTrack &);
+      bool isME0DigiMatched(MyME0Digi me0_dg, MyME0SimHit me0_sh);
+
+      virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
+      virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
+      edm::EDGetToken simTrackInput_;
+      edm::EDGetToken simTrackInput1_;
+      edm::EDGetToken me0SimHitInput_;
+      edm::EDGetToken me0DigiInput_;
+
+    bool debug_;
+    std::string folderPath_;
+    bool EffSaveRootFile_;
+    std::string EffRootFileName_;
+
+    DQMStore * dbe;
+    std::map<std::string, MonitorElement*> meCollection;
+
+    //edm::Handle<edm::PSimHitContainer> ME0Hits;
+    //edm::Handle<ME0DigiPreRecoCollection> me0_digis;
+    //edm::Handle<edm::SimTrackContainer> sim_tracks;
+    //edm::Handle<edm::SimVertexContainer> sim_vertices;
+    edm::ESHandle<ME0Geometry> me0_geom;
+
+    edm::ParameterSet cfg_;
+
+    //edm::InputTag me0SimHitInput_;
+    //edm::InputTag me0DigiInput_;
+    //edm::InputTag simTrackInput_;
+
+    double simTrackMinPt_;
+    double simTrackMaxPt_;
+    double simTrackMinEta_;
+    double simTrackMaxEta_;
+    double simTrackOnlyMuon_;
+
+    const ME0Geometry* me0_geometry_;
+
+    MyME0Digi me0_digi_;
+    MyME0SimHit me0_sh;
+    MySimTrack track_;
+
+    bool hasME0Geometry_;
 
 };
 
-#endif
--- Validation/MuonME0Validation/python/MuonME0Digis_cfi.py
+++ Validation/MuonME0Validation/python/MuonME0Digis_cfi.py
@@ -1,14 +1,22 @@
 import FWCore.ParameterSet.Config as cms
-
+from Validation.MuonME0Validation.simTrackMatching_cfi import SimTrackMatching
 
 me0DigiValidation = cms.EDAnalyzer('ME0DigisValidation',
-    verboseSimHit = cms.untracked.int32(1),
-    stripDigiLabel = cms.InputTag("simMuonME0Digis"),
-    simInputLabel = cms.InputTag('g4SimHits',"MuonME0Hits"),
-    digiInputLabel = cms.InputTag("simMuonME0Digis"),
+    #verboseSimHit = cms.untracked.int32(1),
+    input = cms.InputTag('g4SimHits'),
+    siminput = cms.InputTag('g4SimHits','MuonME0Hits'),
+    digiinput = cms.InputTag('simMuonME0Digis'),
+    debug = cms.untracked.bool(False),
+    folderPath = cms.untracked.string('MuonME0DigisV/ME0DigiTask'),
+    #stripDigiLabel = cms.InputTag("simMuonME0Digis"),
+    #simInputLabel = cms.InputTag('g4SimHits',"MuonME0Hits"),
+    #digiInputLabel = cms.InputTag("simMuonME0Digis"),
     # st1, st2_short, st2_long of xbin, st1,st2_short,st2_long of ybin
-    nBinGlobalZR = cms.untracked.vdouble(80,120),
+    #nBinGlobalZR = cms.untracked.vdouble(80,120),
     # st1 xmin, xmax, st2_short xmin, xmax, st2_long xmin, xmax, st1 ymin, ymax...
-    RangeGlobalZR = cms.untracked.vdouble(515,555,20,160),
-    nBinGlobalXY = cms.untracked.int32(360),
+    #RangeGlobalZR = cms.untracked.vdouble(515,555,20,160),
+    #nBinGlobalXY = cms.untracked.int32(360),
+    EffSaveRootFile = cms.untracked.bool(True),
+    EffRootFileName = cms.untracked.string('ME0Digis_ME.root'),
+    simTrackMatching = SimTrackMatching
 )
